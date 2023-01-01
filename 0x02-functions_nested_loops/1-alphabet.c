09883c8cb95f6be94338b58b61aa5773925fbe59
Joseph-Ajegetina <josephajongbah@gmail.com>
2023-01-01 07:52:08 -0800

    nested functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+		_putchar(i++);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+
+
+/**
+ * _abs - returns absolute value of argument
+ *
+ * @n: signed integer value
+ *
+ * Return: asbolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
