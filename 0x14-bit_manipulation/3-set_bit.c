aab0f376e6812d74048fb8b5d7e5cb6d3f106b8c
Joseph-Ajegetina <josephajongbah@gmail.com>
2023-02-02 15:59:27 -0800

    fix minor bugs

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,10 +7,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 32)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	(*n) |= 1 << index;
-	if (get_bit((*n), index) == 1)
-		return (1);
-	return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
 }
